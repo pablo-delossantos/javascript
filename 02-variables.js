63c04c5ff5935825a02a4c28d18af839cf12117e
Pablo de los Santos <estudiodedalo@gmail.com>
2023-03-30 18:02:53 -0300

    Variables

---
+++ 02-variables.js
@@ -0,0 +1,29 @@
+// Una variable es la representacion de algun lugar en memoria que vamos a usar para guardar un valor.
+
+var nombre = "Pablo"; //Reserva un espacio llamado nombre
+
+// Dos estados de una variable:
+
+// 1. Declarar
+var edad;
+
+// 2. Inicializar
+edad = 44;
+
+// La variable ya tiene un valor, y ya la podemos utilizar.
+edad;
+
+// Variable tipo array
+var elementos = ["Computadora", "Celular"];
+
+// Variable tipo objeto.
+var persona = {
+    nombre: "Pablo",
+    edad: 44,
+};
+
+persona;
+//Guardamos valores de tipo diferente
+
+//elementos[1]
+//celular
