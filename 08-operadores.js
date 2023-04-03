c284a2c0cf39511544d0ff5c08b22b1b324ebacf
Pablo de los Santos <estudiodedalo@gmail.com>
2023-04-03 10:45:17 -0300

    Operadores de operacion, asignacion y comparacion

---
+++ 08-operadores.js
@@ -0,0 +1,44 @@
+// Operadores de operación. Son los que nos van a permitir hacer operaciones matemáticas
+
+//Operadores matematicos o binarios
+
+3 + 2;
+50 - 10;
+10 * 20;
+20 / 2;
+
+"Pablo" + " de los Santos"; // Concatenacion
+
+// Operadores unitarios
+
+!false; // negación
+
+// Operadores de asignacion
+
+var a = 1;
+
+// Operadores de comparación
+
+3 == "3"; // true
+
+3 === "3"; // false
+
+5 < 3; // false
+
+5 > 3; // true
+
+5 <= 6; // true
+
+5 >= 6; // false
+
+// Si a y b
+a && b;
+
+// Si a o b
+a || b;
+
+//Incremento
+var edad = 40;
+edad++; // incrementa en 1
+
+edad += 2; //Incrementa en 2
