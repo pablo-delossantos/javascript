231fa5b269307879af95c782660dc7125f1484fa
Pablo de los Santos <estudiodedalo@gmail.com>
2023-04-02 22:48:56 -0300

    True y false

---
+++ 07-true-false.js
@@ -0,0 +1,25 @@
+// Valores thuthy y falsy (verdadero o falso)
+
+//falsos
+Boolean(0); //false;
+
+Boolean(null); //false;
+
+Boolean(NaN); //false;
+
+Boolean(undefined); //false;
+
+Boolean(""); //false;
+
+// Verdaderos
+Boolean("a"); // true
+
+Boolean(1); //true
+
+Boolean([]); //true cualquier array
+
+Boolean({}); //true cualquier objeto
+
+Boolean(function () {}); //true cualquier funcion
+
+Boolean(true); //true
