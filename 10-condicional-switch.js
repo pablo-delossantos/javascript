56b9acdd7c006966d851eefb587fb58c866943db
Pablo de los Santos <estudiodedalo@gmail.com>
2023-04-20 11:32:44 -0300

    Condicional Switch

---
+++ 10-condicional-switch.js
@@ -0,0 +1,17 @@
+// Es una forma de generar una condición. Este funciona por "casos"
+
+var numero = 1;
+
+switch (numero) {
+    case 1:
+        console.log("Soy uno");
+        break;
+    case 10:
+        console.log("Soy un diez");
+        break;
+    case 100:
+        console.log("Soy un cien");
+        break;
+    default:
+        console.log("No soy nada");
+}
