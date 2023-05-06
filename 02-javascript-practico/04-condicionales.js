bcf5c5e95e069192094671021d9f2f78d5c04f9e
Pablo de los Santos <estudiodedalo@gmail.com>
2023-05-06 16:37:14 +0000

    Archivo clase condicionales

---
+++ 02-javascript-practico/04-condicionales.js
@@ -0,0 +1,11 @@
+// ¿Que es un condicional?
+// Es la forma en que ejecutamos un bloque de código u otro dependiendo de alguna condición o validación.
+
+// ¿Qué tipos de condicionales existen en JavaScript y cuales son sus diferencias?
+// if (else y else if), switch
+
+// El condicional if (con else y else if) nos permite hacer validaciones completamente distintas si hasi queremos en cada validación o condicional. En el switch todos los cases se comparan con la misma variable o condicion que definimos en el switch.
+
+// ¿Se pueden combinar funciones y condicionales?
+// Si. Las funciones pueden encapsular cualquier bloque de código, incluyendo condicionales.
+
