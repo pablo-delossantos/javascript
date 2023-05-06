772acdbc1b685d48fabfa7d58320d8cb5adecb08
Pablo de los Santos <estudiodedalo@gmail.com>
2023-05-06 16:52:18 +0000

    Solucion a los ejercicios del test en el apartado de condicionales

--- 02-javascript-practico/04-condicionales.js
+++ 02-javascript-practico/04-condicionales.js
@@ -9,3 +9,14 @@
 // ¿Se pueden combinar funciones y condicionales?
 // Si. Las funciones pueden encapsular cualquier bloque de código, incluyendo condicionales.
 
+const tipoDeSuscripcion = "Basic"
+
+if (tipoDeSuscripcion == "Free") {
+    console.log("Solo puedes tomar los cursos gratis");
+} else if (tipoDeSuscripcion == "Basic") {
+    console.log("Puedes tomar todos los cursos de Platzi durante un mes");
+} else if (tipoDeSuscripcion == "Expert") {
+    console.log("Puedes tomar todos los cursos de Platzi durante un año");
+} else if (tipoDeSuscripcion == "Expert") {
+    console.log("Tu y alguién mas pueden tomar todos los cursos de Platzi durante un año");
+}
