db5d77bb9f77eac56fdb06446cfa0c6e6d9f5e38
Pablo de los Santos <estudiodedalo@gmail.com>
2023-05-12 10:32:05 -0300

    Bonus conversion de if a objetos

--- 02-javascript-practico/07-bonus.js
+++ 02-javascript-practico/07-bonus.js
@@ -42,4 +42,24 @@ function conseguirTipoSuscripcion(suscripcion) {
 conseguirTipoSuscripcion("Expert")
 // Puedes tomar todos los cursos de Platzi durante un año.
 
-// Bonus: si ya eres una experta o experto en este lenguaje, te desafío a comentar cómo replicar este comportamiento con arrays y un sólo condicional.
+// Bonus: si ya eres una experta o experto en este lenguaje, te desafío a comentar cómo replicar este comportamiento con arrays o objetos y un sólo condicional.
+
+// Primero creamos un objeto con las suscripciones:
+const tiposDeSuscripciones = {
+    free: "Sólo puedes tomar los cursos gratis",
+    basic: "Puedes tomar casi todos los cursos de Platzi durante un mes",
+    expert: "Puedes tomar casi todos los cursos de Platzi durante un año",
+    expertduo: "Tu y alguien más pueden tomar todos los cursos de Platzi durante un año",
+};
+
+// Luego creamos una funcion que llama al objeto:
+function conseguirTipoSuscripcion(suscripcion){
+    if (tiposDeSuscripciones[suscripcion]) {
+        console.log(tiposDeSuscripciones[suscripcion])
+        return;
+    }
+    console.warn("Este tipo de suscripción no existe")
+}
+
+conseguirTipoSuscripcion("expertduo")
+// Tu y alguien más pueden tomar todos los cursos de Platzi durante un año
